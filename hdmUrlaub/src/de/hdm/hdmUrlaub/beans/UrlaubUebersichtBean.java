e60a4892e3a8880c15510c95010f742e009bf2a0
Fabian Röber <fabianroeber@aol.com>
2015-01-10 14:18:54 +0100

    Beans für Uralaubsantrag und Status

---
+++ hdmUrlaub/src/de/hdm/hdmUrlaub/beans/UrlaubUebersichtBean.java
@@ -0,0 +1,10 @@
+package de.hdm.hdmUrlaub.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "urlaubUebersichtBean")
+@SessionScoped
+public class UrlaubUebersichtBean {
+
+}
---
+++ hdmUrlaub/src/de/hdm/hdmUrlaub/beans/UrlaubsAntragBean.java
@@ -0,0 +1,10 @@
+package de.hdm.hdmUrlaub.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean(name = "urlaubsantragBean")
+@ViewScoped
+public class UrlaubsAntragBean {
+
+}
