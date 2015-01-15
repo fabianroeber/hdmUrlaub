3dc50a031695426717798fb056d6835a816a1af9
Fabian Röber <fabianroeber@aol.com>
2015-01-15 14:59:16 +0100

    Auslagerung Urlaubsantrag + urlaubsübersicht LDAP SDK Login Page

--- hdmUrlaub/src/de/hdm/hdmUrlaub/beans/UserBean.java
+++ hdmUrlaub/src/de/hdm/hdmUrlaub/beans/UserBean.java
@@ -3,7 +3,7 @@ package de.hdm.hdmUrlaub.beans;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
-@ManagedBean (name="userBean")
+@ManagedBean(name = "userBean")
 @SessionScoped
 public class UserBean {
 
